dee164b059ab8fd7ad74b61101a571a86500eddc
sione-tenifa <johntenifa@yahoo.com>
2021-05-28 10:12:41 -0600

    run

--- app.py
+++ app.py
@@ -27,33 +27,39 @@ def lucky_num():
     year = request.json["year"]
     color = request.json["color"]
     email = request.json["email"]
-
-    if email not in request.json or request.json["email"] is "":
+    
+    print(name)
+    
+    if "email" not in request.json or request.json["email"] is "":
         errors["error"]["email"] = "Email is required"
-    if name not in request.json or request.json["name"] is "":
+    if "name" not in request.json or request.json["name"] is "":
         errors["error"]["name"] = "Name is required."
-    if year not in request.json or request.json["year"] is "":
+    if "year" not in request.json or request.json["year"] is "":
         errors["error"]["year"] = "Year is required."
     elif int(request.json["year"]) < 1900 or int(request.json["year"])> 2000:
         errors["error"]["year"] = "Year must be between 1900 and 2000, inclusive."
-    if color not in request.json or request.json["color"] is "":
+    if "color" not in request.json or request.json["color"] is "":
         errors["error"]["color"] = "Color is required."
     elif request.json["color"].lower() not in valid_colors:
         errors["error"]["color"] = "Invalid color."
 
     if len(errors["error"]) != 0:
+        print(errors["error"])
         res_obj = errors
         res_json = jsonify(res_obj)
         return res_json
 
     else:
+        
         random_fact = requests.get(f'http://numbersapi.com/{random_num}/trivia')
+        print(random_fact)
         input_year = requests.get(f'http://numbersapi.com/{year}/year')
 
         res_obj = {"num" : {"fact" : random_fact.text, "num" : random_num},
                            "year" : {"fact" : input_year.text, "year" : year }
                           }
         res_json = jsonify(res_obj)
+        print(res_json)
         return res_json
 
 
